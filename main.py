26f55941d47a433a4de8ff38b7a9b21b7996ae50
Nikolay Savchenko <7548568@gmail.com>
2022-10-30 22:42:27 +0300

    Rename variables

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import randint
 
 from dotenv import load_dotenv
 
-from utils import get_comics
+from utils import get_comic
 from utils import get_group_detail
 from utils import get_url_for_upload_photo
 from utils import publication_comics
@@ -16,18 +16,18 @@ def main():
     load_dotenv()
     vk_token = os.environ['VK_TOKEN']
     Path('comics').mkdir(parents=True, exist_ok=True)
-    comics_number = randint(1, 2689)
-    comics = get_comics(comics_number)
-    comics_image_name = comics[1]
-    comics_comment = comics[0]
+    comic_number = randint(1, 2689)
+    comic = get_comic(comic_number)
+    comic_image_name = comic[1]
+    comic_comment = comic[0]
     vk_group_id = get_group_detail(vk_token)
     upload_photo_url = get_url_for_upload_photo(vk_token, vk_group_id)
-    upload_details = upload_photo_VK(Path(f'comics/{comics_image_name}'), upload_photo_url)
+    upload_details = upload_photo_VK(Path(f'comics/{comic_image_name}'), upload_photo_url)
     saving_photo_details = save_photo_VK(vk_token, upload_details, vk_group_id)
     publication_comics(vk_token, saving_photo_details['response'][0]['id'],
                        saving_photo_details['response'][0]['owner_id'],
-                       comics_comment, vk_group_id)
-    os.remove(f'comics/{comics_image_name}')
+                       comic_comment, vk_group_id)
+    os.remove(f'comics/{comic_image_name}')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -10,18 +10,17 @@ def get_group_detail(token):
     return response.json()['response']['items'][0]['id']
 
 
-def get_comics(comics_number):
-    Path('comics').mkdir(parents=True, exist_ok=True)
-    url = f'https://xkcd.com/{comics_number}/info.0.json'
+def get_comic(comic_number):
+    url = f'https://xkcd.com/{comic_number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    comics_details = response.json()
-    response_image = requests.get(comics_details['img'])
+    comic_details = response.json()
+    response_image = requests.get(comic_details['img'])
     response_image.raise_for_status()
-    image_name = (comics_details['img'].split('/'))[-1]
+    image_name = (comic_details['img'].split('/'))[-1]
     with open(Path(f'comics/{image_name}'), 'wb') as file:
         file.write(response_image.content)
-    return comics_details['alt'], image_name
+    return comic_details['alt'], image_name
 
 
 def get_url_for_upload_photo(token, group_id):
